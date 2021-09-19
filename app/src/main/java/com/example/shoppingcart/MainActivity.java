a50ae0d77b76042f38675e722eeda60315a9daf7
bpreise1 <47012838+bpreise1@users.noreply.github.com>
2021-09-19 17:12:22 -0400

    Changing views and item list

--- app/src/main/java/com/example/shoppingcart/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.shoppingcart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.shoppingcart.databinding.ActivityMainBinding;
+import com.example.shoppingcart.databinding.AddItemBinding;
+import com.example.shoppingcart.databinding.ItemLayoutBinding;
+
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private AddItemBinding addItemBinding;
+
+    Vector<String> list = new Vector<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        addItemBinding = AddItemBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void displayAddItem(View view) {
+        View v = addItemBinding.getRoot();
+        setContentView(v);
+    }
+
+    public void addItemToList(View view) {
+        list.add(addItemBinding.item.getText().toString());
+        View v = binding.getRoot();
+        setContentView(v);
+        logList();
+    }
+
+    public void logList() {
+        for(String item : list) {
+            System.out.println(item);
+        }
     }
 }
